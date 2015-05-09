69cafcd0b73a0efefe022e66d8e4a7a15598d582
Ankit <ankituma@usc.edu>
2015-05-09 16:36:07 -0700

    changes in schema

--- src/jsonToDB.py
+++ src/jsonToDB.py
@@ -5,18 +5,34 @@ conn = sqlite3.connect('facebook.db')
 c = conn.cursor()
 
 '''
+c.execute("DROP TABLE PERSON")
+c.execute("DROP TABLE EDUCATION")
+c.execute("DROP TABLE ATHLETE")
+c.execute("DROP TABLE TEAM")
+c.execute("DROP TABLE SPORT")
+c.execute("DROP TABLE MUSIC")
+c.execute("DROP TABLE BOOKS")
+c.execute("DROP TABLE MOVIES")
+c.execute("DROP TABLE INS_PEOPLE") ##for inspirationa people
+
+#NEW SCHEMA:
+
 c.execute("CREATE TABLE PERSON (name text , birthday text, hometown text, location text)")
 c.execute("CREATE TABLE EDUCATION (name text , school_name text, school_type text, school_year text)")
 c.execute("CREATE TABLE ATHLETE (name text , athlete text)")
 c.execute("CREATE TABLE TEAM (name text , team text)")
 c.execute("CREATE TABLE SPORT (name text , sports text)")
-c.execute("CREATE TABLE MUSIC (name text , category text, music_name text)")
-c.execute("CREATE TABLE BOOKS (name text , category text, books_name text)")
-c.execute("CREATE TABLE MOVIES (name text , category text, movies_name text)")
-c.execute("CREATE TABLE INS_PEOPLE (name text , people text)")
+c.execute("CREATE TABLE MUSIC (name text , category text, music text)")
+c.execute("CREATE TABLE BOOKS (name text , category text, books text)")
+c.execute("CREATE TABLE MOVIES (name text , category text, movie text)")
+c.execute("CREATE TABLE INS_PEOPLE (name text , people text)") ##for inspirationa people
+
 conn.commit()
+conn.close()
+
 '''
 
+#'''
 with open('final.json') as data_file:    
     data = json.load(data_file)
 
@@ -181,3 +197,4 @@ for records in data:
 
 conn.commit()
 conn.close()
+#'''
