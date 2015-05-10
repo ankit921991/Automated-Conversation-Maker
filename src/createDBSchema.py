efcfd635a6bae220d5740851158b12c80c0aa71f
Ankit <ankituma@usc.edu>
2015-05-09 17:37:59 -0700

    file with documentation

--- src/createDBSchema.py
+++ src/createDBSchema.py
@@ -4,7 +4,19 @@ import re
 
 def generateDBSchema():
     """
-    This function drops the tables if they are already present and then generates them from the respective create table commands.The databse used is sqlite3
+    This extracts the data from the JSON data which is obtained by using the Facebook graph search API.The sample JSON data can be found at the github repository at : https://github.com/ankit921991/Automated-Conversation-Maker/tree/master/data.This data obtained can be categorisez into following cattegories
+
+1) Personal - Database Table :: PERSON. Fields :: name, birthday, hometown, location 
+2) Educational - Database Table :: EDUCATION. Fields :: name, school_name, school_type, school_year
+3) Music - Database Table :: MUSIC. Fields :: name, category, music
+4) books - Database Table :: BOOKS. Fields :: name, category, books
+5) movies - Database Table :: MOVIES. Fields :: name, category, movie
+6) sports - Database Table :: SPORT. Fields :: name, sports
+7) favorite team - Database Table :: TEAM. MOVIESFields :: name, team
+8) favorite athlete - Database Table :: ATHLETE. Fields :: name, athlele
+9) Inspirational  people - Database Table :: INS_PEOPLE. Fields :: name, people
+
+This program extracts the data from the JSON file and categorize them into above categorisies. Then the data is inserted into the database into respective tables as shown above.  This data will be used later by virtual humans for communication. Here "name" field is there in all thee tables. Though there is no primary key for any of the table, "name" field virtually acts as a primary key to extract the data
     """
     conn = sqlite3.connect('facebook.db')
     c = conn.cursor()
